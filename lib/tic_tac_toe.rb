ccfb5f89845932e92c829be078078a7dbb759aae
Paulina Salem <paulinasalem@yahoo.com>
2018-04-05 21:41:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -88,7 +88,7 @@ def turn(board)
   input = gets
 index = input_to_index(input)
   if valid_move?(board, index)
-    puts move(board, index, value = 'X' || value = 'O')
+    move(board, index)
     puts display_board(board)
   else
     puts "Please enter 1-9:"
